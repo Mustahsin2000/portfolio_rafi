8b03d73a88ba7fd0b9ba0286fafcc4ebda8ff70f
mustahsin2000 <mustahsin152415@gmail.com>
2023-01-14 18:19:30 +0600

    works

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 ## Available Scripts
 
-In the project directory, you can run:
-
+In the project we use:
+1. React js
+2. Sass
+3. Metarial UI
 ### `npm start`
 
 Runs the app in the development mode.\
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useEffect, useRef } from 'react'
 import './Intro.scss'
+import { init } from 'ityped'
 export default function Intro() {
+  
+  const textRef = useRef();
+
+  useEffect(()=>{
+    init(textRef.current, { 
+      showCursor: true,
+      backDelay:1000,
+      backSpeed:50,
+      strings: ["Developer", "Designer" , "Researcher"], 
+      
+    });
+  },[]);
+  
   return (
     <div className='Intro' id='Intro'>
-      
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/raffffi.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+          <div className="wrapper">
+            <h2>Hi There, I'm</h2>
+            <h1>Mustahsin Al Rafi</h1>
+            <h3>Web <span ref={textRef}></span>
+            </h3>
+          </div>
+          <a href="#Portfolio">
+            <img src="assets/down.png" alt="" />
+          </a>
+      </div>
+
     </div>
-  )
+  );
 }
